6e879580822904e610062971c0f79343f2f0317c
Alex Yakir <studio@alexyakir.com>
2016-05-21 19:55:16 +0300

    Added testimonials content

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,2 +1,6 @@
-var signupform = document.getElementById('signup-form');
-signupform.setAttribute('action', '//formspree.io/' + 'alex' + '@' + 'creativecoding' + '.' + 'co.il');
+$(function() {
+  if ($('main').is('.signup-page')) {
+    var signupform = document.getElementById('signup-form');
+    signupform.setAttribute('action', '//formspree.io/' + 'alex' + '@' + 'creativecoding' + '.' + 'co.il');
+  }
+});
