5b075429622e1a57033ec97f07207e761cf1bf17
Alex Yakir <studio@alexyakir.com>
2016-05-22 18:18:49 +0300

    building contact form

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,13 @@
 $(function() {
   if ($('main').is('.signup-page')) {
-    var signupform = document.getElementById('signup-form');
+    var signupform = document.getElementById('form');
+    signupform.setAttribute('action', '//formspree.io/' + 'alex' + '@' + 'creativecoding' + '.' + 'co.il');
+  }
+});
+
+$(function() {
+  if ($('main').is('.contact-page')) {
+    var signupform = document.getElementById('form');
     signupform.setAttribute('action', '//formspree.io/' + 'alex' + '@' + 'creativecoding' + '.' + 'co.il');
   }
 });
